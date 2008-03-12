294362b77d2b129e8cc9ae1a04c811eca06d8c7f
thethom <thethom@0fabbd90-9a41-0410-b4ba-1f92f86c8621>
2008-03-12 06:04:47 +0000

    Progress to Generate Namelist for ndown as far as &domains

--- wrf/pydown/utils.py
+++ wrf/pydown/utils.py
@@ -34,7 +34,7 @@ def generate_namelist_input_dpp_real(pp,namelist_input_master,run_dir='.'):
     """
 
     namelist_dict=wu.read_namelist(namelist_input_master)
-    idx_discard=range(namelist_dict['&domains']['max_dom'][0])
+    idx_discard=range(int(namelist_dict['&domains']['max_dom'][0]))
     
     # We'll be using this list as the list of entries to 'pop' from each variable 
     # in the namelist, so we need ot reverse siht list to preserve only the 'pp'th
@@ -50,19 +50,59 @@ def generate_namelist_input_dpp_real(pp,namelist_input_master,run_dir='.'):
 	    else:
 		for idx in idx_discard:
 		    dummy.pop(idx)
+   # TODO
+    # Shorten the time extent to save on CPU resources (ref. step 15 in dia)
+    # Include explicit definition of eta_levels
+
+    wu.write_namelist(namelist_dict,os.path.join(run_dir,'namelist.input.d'+str(pp).zfill(2)+'.real'))
+    return None
+
+def generate_namelist_input_dpp_ndown(pp,namelist_input_master,run_dir='.'):
+    """Generate a namelist for wrf.exe for the pp'th domain
     
-    wu.write_namelist(namelist_dict,os.path.join(run_dir,'namelist.input.d'+str(pp).zfill(2)+'.wrf'))
+    This namelist will contain only one column, with max_dom = 1 and 
+    interval_seconds = grib interval
+    """
 
+    namelist_dict=wu.read_namelist(namelist_input_master)
+    
+    # Correcting to give BC's at time of history_interval for parent grid. 
+    # history_interval given in minutes, interval_seconds, in seconds (duh??)
+    dom_idx = pp-1 
+    history_interval = namelist_dict['&time_control']['history_interval'][dom_idx-1]
+    interval_seconds = history_interval*60
+    namelist_dict['&time_control']['interval_seconds'][0]=interval_seconds
+    
+    idx_discard=range(int(namelist_dict['&domains']['max_dom'][0]))
+    
+    # We'll be using this list as the list of entries to 'pop' from each variable 
+    # in the namelist, so we need ot reverse siht list to preserve only the 'pp'th
+    # datum
+    idx_discard.pop(idx_discard.index(dom_idx))
+    idx_discard.pop(idx_discard.index(dom_idx-1))
+    idx_discard.reverse()
 
+    for group in namelist_dict.keys():
+	for variable in namelist_dict[group].keys():
+	    dummy=namelist_dict[group][variable]
+	    if len(dummy)==1:
+		pass
+	    else:
+		for idx in idx_discard:
+		    dummy.pop(idx)
 
+    namelist_dict['&domains']['max_dom'][0]=2
+    # ndown reads 1 as parent and 2 as child hence
+    namelist_dict['&domains']['parent_id']=[1,2]
+    # parent grid ratio of 'parent' is 1:
+    namelist_dict['&domains']['parent_grid_ratio'][0]=1
+    # j_parent_start of parent is 1
+    namelist_dict['&domains']['j_parent_start'][0]=1
+    # same for i_parent_start
+    namelist_dict['&domains']['i_parent_start'][0]=1
 
-
-
-
-
-
+    wu.write_namelist(namelist_dict,os.path.join(run_dir,'namelist.input.d'+str(pp).zfill(2)+'.ndown'))
     return None
+
 def generate_namelist_input_dpp_wrf():
-    return None
-def generate_namelist_input_dpp_ndown():
     return None
